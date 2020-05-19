2a94741c7f0b7a802b384a3f589567a1d6d00785
vuvk <panknd@mail.ru>
2020-05-19 12:40:52 +0500

    configured project to build gir and typelib

--- build.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-meson build --buildtype=debug && ninja -C build
-cd build
-cp Bodhi@sha/Bodhi-0.1.gir .
-g-ir-compiler --shared-library=libBodhi.so --output=Bodhi-0.1.typelib Bodhi-0.1.gir
